c8ae14d2307aa1c09db4a036788742ed1b5b0b18
niklasberlin <github@niklas-aumueller.de>
2024-04-25 00:01:13 +0200

    initial changes

--- moduleScript.js
+++ moduleScript.js
@@ -1,7 +1,14 @@
 function init() {
   script.log("Custom module init");
+  local.send('< GET 0 ALL >');
+  //Message received : < GET 0 ALL >< SET 0 METER_RATE 5000 > //companion init
 }
 
+function dataReceived(data) {
+
+  script.log("Received data : "+data);
+  
+  }
 
 function moduleParameterChanged(param) {
   script.log(param.name + " parameter changed, new value: " + param.get());
