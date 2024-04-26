c476c7170c1f2b14001de21e0e630834a0dacb62
Niklas Aumüller <git@niklas-aumueller.de>
2024-04-26 03:30:22 +0200

    added command definitions

--- moduleScript.js
+++ moduleScript.js
@@ -1,7 +1,56 @@
+var flashtime = 4;  //time the flashing indicator stays lit
+var device_flashtime = 0;
+var channel_1_flashtime = 0;
+var channel_2_flashtime = 0;
+var todo = false;
+
 function init() {
-  script.log("Custom module init");
-  //setup all value fields
+  script.setUpdateRate(1);
+  //request all value fields
   getAll();
+}
+
+function update(delta){
+  script.log("UPDATE LOOP");
+  if (local.values.device.flashing.get()){
+    device_flashtime +=1;
+    if (device_flashtime >= flashtime){
+      device_flashtime = 0;
+      local.values.device.flashing.set(0);
+    }
+  }
+  if (local.values.channel1.flashing.get()){
+    channel_1_flashtime +=1;
+    if (channel_1_flashtime >= flashtime){
+      channel_1_flashtime = 0;
+      local.values.channel1.flashing.set(0);
+    }
+  }
+  if (local.values.channel2.flashing.get()){
+    channel_2_flashtime +=1;
+    if (channel_2_flashtime >= flashtime){
+      channel_2_flashtime = 0;
+      local.values.channel2.flashing.set(0);
+    }
+  }
+
+
+}
+
+function toInt(input) {
+  //function is used to parse strings with leading 0 to int, parseInt assumes a number in octal representation due to the leading 0, so 05000 becomes 2560. with this function 05000 will be parsed as 5000.
+  string = input;
+  notNull = false;
+  res = "";
+  for (indx = 0; indx < string.length; indx++) {
+    char = string.substring(indx, indx + 1);
+    if (char != 0 || notNull) {
+      res += char;
+      notNull = true;
+    }
+  }
+
+  return parseInt(res);
 }
 
 function dataReceived(inputData) {
@@ -52,18 +101,17 @@ function dataReceived(inputData) {
         local.values.device.lockStatus.setData(parts[2]);
       }
       if (parts[1] == "FLASH") {
-        if(parts[2]=="ON"){
+        if (parts[2] == "ON") {
           local.values.device.flashing.set(true);
-        }else{
+        } else {
           local.values.device.flashing.set(false);
         }
-        
       }
       //CHANNEL INFOS
       if (parts[2] == "FLASH") {
-        if (parts[3] == "ON"){
-        local.values.getChild("channel" + parts[1]).flashing.set(true);
-        }else{
+        if (parts[3] == "ON") {
+          local.values.getChild("channel" + parts[1]).flashing.set(true);
+        } else {
           local.values.getChild("channel" + parts[1]).flashing.set(false);
         }
       }
@@ -80,11 +128,11 @@ function dataReceived(inputData) {
       if (parts[2] == "GROUP_CHANNEL") {
         grp_info = string.split(",");
         //root.modules.shureSLX_D.values.channel1.rfGroup
-        if (grp_info[0] == "--"){
-          grp_info[0]=0;
+        if (grp_info[0] == "--") {
+          grp_info[0] = 0;
         }
-        if (grp_info[1] == "--"){
-          grp_info[1]=0;
+        if (grp_info[1] == "--") {
+          grp_info[1] = 0;
         }
         local.values
           .getChild("channel" + parts[1])
@@ -94,16 +142,15 @@ function dataReceived(inputData) {
           .rfChannel.set(parseInt(grp_info[1]));
       }
       if (parts[2] == "AUDIO_GAIN") {
-        if(parts[3][0]==0){
-          parts[3] = parts[3].substring(1,parts[3].length);
+        if (parts[3][0] == 0) {
+          parts[3] = parts[3].substring(1, parts[3].length);
         }
         //root.modules.shureSLX_D.parameters.updateRateCh1
         local.values
           .getChild("channel" + parts[1])
-          .audioGain.set(parseInt(parts[3])- 18);
+          .audioGain.set(parseInt(parts[3]) - 18);
       }
       if (parts[2] == "AUDIO_LEVEL_RMS") {
-
         local.values
           .getChild("channel" + parts[1])
           .audioLevelRMS.set(parseInt(parts[3]) - 120);
@@ -119,7 +166,7 @@ function dataReceived(inputData) {
           local.values
             .getChild("channel" + parts[1])
             .rssiAntA.set(parseInt(parts[4]) - 120);
-        }else if(parts[1] == 2) {
+        } else if (parts[1] == 2) {
           local.values
             .getChild("channel" + parts[1])
             .rssiAntB.set(parseInt(parts[4]) - 120);
@@ -142,15 +189,15 @@ function dataReceived(inputData) {
       if (parts[2] == "FREQUENCY") {
         //root.modules.shureSLX_D.parameters.updateRateCh1
         //root.modules.shureSLX_D.values.channel1.audioLevelRMS
-        dec = parts[3].substring(parts[3].length-3,parts[3].length);
-        lead = parts[3].substring(0,parts[3].length-3);
-        if(lead[0]==0){
-          lead = lead.substring(1,lead.length);
+        dec = parts[3].substring(parts[3].length - 3, parts[3].length);
+        lead = parts[3].substring(0, parts[3].length - 3);
+        if (lead[0] == 0) {
+          lead = lead.substring(1, lead.length);
         }
 
         local.values
           .getChild("channel" + parts[1])
-          .frequency.set(lead+"."+dec);
+          .frequency.set(lead + "." + dec);
       }
       if (parts[2] == "TX_BATT_BARS") {
         //root.modules.shureSLX_D.values.channel1.batteryBars
@@ -167,25 +214,23 @@ function dataReceived(inputData) {
       if (parts[2] == "TX_BATT_MINS") {
         //root.modules.shureSLX_D.values.channel1.batteryBars
         mins = parseInt(parts[3]);
-        if(mins<=65532){
-          hrs = Math.floor(mins/60);
-          min = mins-hrs*60;
-          lbl = hrs+" hrs "+min+" min";
-        }else if(mins == 65533){
+        if (mins <= 65532) {
+          hrs = Math.floor(mins / 60);
+          min = mins - hrs * 60;
+          lbl = hrs + " hrs " + min + " min";
+        } else if (mins == 65533) {
           lbl = "Battery communication warning";
-        }else if(mins == 65534){
+        } else if (mins == 65534) {
           lbl = "Battery time calculating";
-        }else if(mins == 65535){
+        } else if (mins == 65535) {
           lbl = "UNKNOWN";
         }
-        local.values
-          .getChild("channel" + parts[1])
-          .batteryRuntime.set(lbl);
+        local.values.getChild("channel" + parts[1]).batteryRuntime.set(lbl);
       }
     } else if (parts[0] == "SAMPLE") {
       //this is sample Data
       //TODO: do something with it
-      if(parts[2]=="ALL"){
+      if (parts[2] == "ALL") {
         //Level Peak
         local.values
           .getChild("channel" + parts[1])
@@ -199,27 +244,53 @@ function dataReceived(inputData) {
           local.values
             .getChild("channel" + parts[1])
             .rssiAntA.set(parseInt(parts[5]) - 120);
-        }else if(parts[1] == 2) {
+        } else if (parts[1] == 2) {
           local.values
             .getChild("channel" + parts[1])
             .rssiAntB.set(parseInt(parts[5]) - 120);
         }
-
       }
     }
   }
 }
 
 function moduleParameterChanged(param) {
-  script.log(param.name + " parameter changed, new value: " + param.get());
+  //script.log(param.name + " parameter changed, new value: " + param.get());
   //root.modules.shureSLX_D.parameters.output.isConnected
-  if(param.name=="isConnected" && param.get()==1){
+  if (param.name == "isConnected" && param.get() == 1) {
     getAll();
+  }
+  if (param.name == "updateRateCh1") {
+    setMeterRate(1, toInt(param.get()));
+  }
+  if (param.name == "updateRateCh2") {
+    setMeterRate(2, toInt(param.get()));
   }
 }
 
 function moduleValueChanged(value) {
   script.log(value.name + " value changed, new value: " + value.get());
+  script.log("parent element: " + value.getParent().name);
+  if (value.getParent().name == "device") {
+    if (value.name == "flashing" && value.get() == 1) {
+      setFlashing(0);
+    }
+    if (value.name == "deviceID") {
+      setDeviceID(value.get());
+    }
+  }
+  if (value.getParent().name.substring(0, 7) == "channel") {
+    channel = value.getParent().name.substring(7, 8);
+    if (value.name == "flashing" && value.get() == 1) {
+      setFlashing(channel);
+    }
+    if (value.name == "name") {
+      setChannelName(channel, value.get());
+    }
+    if (value.name == "audioGain") {
+      setAudioGain(channel, value.get());
+    }
+  }
 }
 
 function requestModel() {
@@ -277,53 +348,52 @@ function getAll() {
   //Message received : < GET 0 ALL >< SET 0 METER_RATE 5000 > //companion init
 }
 
-function setFlashing(ch){
-  if(typeof(ch)=="undefined"){
+function setFlashing(ch) {
+  if (typeof ch == "undefined" || ch == 0) {
     local.send("< SET FLASH ON >");
-  }else if(ch==1 || ch==2){
-    local.send("< SET "+ch+" FLASH ON >");
+  } else if (ch == 1 || ch == 2) {
+    local.send("< SET " + ch + " FLASH ON >");
   }
 }
 
-function setDeviceID(newid){
-  local.send("< SET DEVICE_ID {"+newid+"} >");
+function setDeviceID(newid) {
+  local.send("< SET DEVICE_ID {" + newid + "} >");
 }
 
-function setChannelName(ch,newName){
-  if(ch == 1 || ch == 2){
-    local.send("< SET "+ch+" CHAN_NAME {"+newName.substring(0,8)+"} >");
+function setChannelName(ch, newName) {
+  if (ch == 1 || ch == 2) {
+    local.send(
+      "< SET " + ch + " CHAN_NAME {" + newName.substring(0, 8) + "} >"
+    );
   }
 }
 
-function setAudioGain(ch, gain){
+function setAudioGain(ch, gain) {
   //< SET x AUDIO_GAIN 40 >
-  if(ch == 1 || ch == 2){
-    local.send("< SET "+ch+" AUDIO_GAIN "+(gain+18)+" >");
+  if (ch == 1 || ch == 2) {
+    local.send("< SET " + ch + " AUDIO_GAIN " + (gain + 18) + " >");
   }
 }
 
-function incAudioGain(ch, addgain){
+function incAudioGain(ch, addgain) {
   //< SET x AUDIO_GAIN 40 >
-  if(ch == 1 || ch == 2){
-    local.send("< SET "+ch+" AUDIO_GAIN INC "+(addgain)+" >");
+  if (ch == 1 || ch == 2) {
+    local.send("< SET " + ch + " AUDIO_GAIN INC " + addgain + " >");
   }
 }
 
-function decAudioGain(ch, addgain){
+function decAudioGain(ch, addgain) {
   //< SET x AUDIO_GAIN 40 >
-  if(ch == 1 || ch == 2){
-    local.send("< SET "+ch+" AUDIO_GAIN DEC "+(addgain)+" >");
+  if (ch == 1 || ch == 2) {
+    local.send("< SET " + ch + " AUDIO_GAIN DEC " + addgain + " >");
   }
 }
 
-function setMeterRate(ch,rate){
+function setMeterRate(ch, rate) {
+  script.log("Function setMeterRate called with ch: "+ch+" | rate: "+rate);
+  rate = toInt(rate);
   //< SET x METER_RATE 01000 >
-  if((ch == 1 || ch == 2) && ((rate >= 100 && rate <=65535) || rate == 0)){
-    local.send("< SET "+ch+" METER_RATE "+rate+" >");
+  if ((ch == 1 || ch == 2) && ((rate >= 100 && rate <= 65535) || rate == 0)) {
+    local.send("< SET " + ch + " METER_RATE " + rate + " >");
   }
-}
-
-// This is the callback function for the "Custom command" command
-function customCmd(val) {
-  script.log("Custom command called with value " + val);
 }
